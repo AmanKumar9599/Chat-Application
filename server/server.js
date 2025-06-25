6ca602d2431e10902d015796c6f6ffa14fe8c7ee
Aman Kumar <amankuntal9599@gmail.com>
2025-06-25 17:39:18 +0530

    Fixed cors issue

--- server/server.js
+++ server/server.js
@@ -15,10 +15,22 @@ app.use(cookieParser());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://keen-pudding-ed8899.netlify.app"
+];
+
 app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
 }));
+
 
 // DB Connect
 connectDB();
